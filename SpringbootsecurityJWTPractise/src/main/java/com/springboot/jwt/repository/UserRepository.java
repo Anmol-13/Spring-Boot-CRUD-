7d2f6af0ee7e08b81a9db4e51ab42f06a0d3f2ae
unknown <anmoldeepsingh1002@gmail.com>
2022-10-05 23:52:45 +0530

    Jwt Token Impl

---
+++ SpringbootsecurityJWTPractise/src/main/java/com/springboot/jwt/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.jwt.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+	User findByEmailIgnoreCase(String username);
+
+}
